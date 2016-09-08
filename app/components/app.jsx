51b669d20775cb49d7c7b2f0ccbad5ffb967257e
James Conkling <james.lane.conkling@gmail.com>
2016-09-08 17:12:28 -0400

    remove test code

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,43 +1,11 @@
 'use strict';
 import React from 'react';
-import SearchForm from './search-form.jsx';
 
 export default React.createClass({
-  getInitialState() {
-    return {
-      items: [
-        {id: 1, name: 'one', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
-        {id: 2, name: 'two', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
-        {id: 3, name: 'three', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
-        {id: 4, name: 'four', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
-        {id: 5, name: 'five', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
-        {id: 6, name: 'six', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
-        {id: 7, name: 'seven', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}
-      ],
-      queryString: null
-    };
-  },
-
-  updateQueryString(queryString) {
-    this.setState({queryString});
-  },
-
-  deleteItem(deletedItemId) {
-    this.setState({
-      items: this.state.items.filter(item => item.id !== deletedItemId) 
-    });
-  },
-
   render() {
     return (
       <section>
         <h1>REACT BOILER</h1>
-        <SearchForm queryString={this.state.queryString} updateQueryString={this.updateQueryString} />
-
-        {this.props.children && React.cloneElement(this.props.children, { 
-          items: this.state.items, 
-          deleteItem: this.deleteItem
-        })}
       </section>
     );
   }
--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,13 @@
 'use strict';
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router'; 
+import { Router, Route, hashHistory } from 'react-router';
 import App from './components/app.jsx';
-import Results from './components/results.jsx';
 
 render((
   <Router history={hashHistory}>
-    <Route path='/' component={App} >
-      <Route path='/results' component={Results}/>
-    </Route>
+    <Route path='/' component={App} />
   </Router>
 ), document.getElementById('app'));
-
-if (process.env.NODE_ENV !== 'production') {
-  window.React = React;
-}
 
 export default App;
