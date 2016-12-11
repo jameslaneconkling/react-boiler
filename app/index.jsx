c3e3b9d9b7680d2ffc5be4841116112e84bfeef4
James Conkling <james.lane.conkling@gmail.com>
2016-12-11 16:29:27 -0500

    import style

--- app/index.jsx
+++ app/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React                  from 'react';
 import { render }             from 'react-dom';
 import {
@@ -12,6 +13,7 @@ import { createStore }        from 'redux';
 import { Provider }           from 'react-redux';
 import reducer                from './reducers/index';
 import App                    from './components/app';
+import                             './style.scss';
 
 const store = createStore(reducer, window.devToolsExtension && window.devToolsExtension());
 
