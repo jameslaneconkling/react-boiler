0ddb1b46d16825da3235cae0ef5f39da8c8aab60
James Conkling <james.lane.conkling@gmail.com>
2018-07-16 20:38:42 -0400

    remove eslint dev deps

--- webpack.common.js
+++ webpack.common.js
@@ -13,7 +13,7 @@ module.exports = {
   ],
 
   output: {
-    filename: '[name].[chunkhash].js',
+    filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
   },
