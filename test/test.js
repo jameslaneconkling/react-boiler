c8a7be12c636f7117e6778f99dc1b76c6c19270b
James Conkling <james.lane.conkling@gmail.com>
2018-11-26 12:45:10 -0500

    update test runner to use typescript

--- test/test.js
+++
@@ -1,8 +0,0 @@
-import test from 'tape';
-import { identity } from '../src/utils/utils';
-
-test('placeholder', (t) => {
-  t.plan(1);
-
-  t.equal('xxx', identity('xxx'));
-});
---
+++ test/test.ts
@@ -0,0 +1,7 @@
+import test from 'tape';
+
+
+test('Stub', (assert) => {
+  assert.plan(1);
+  assert.equal(1, 1);
+});
